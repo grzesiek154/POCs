3da1eba2f9273a2725cebb0097f6fc862b327ec4
grzesiek <g.malarski@samsung.com>
2019-07-19 13:34:30 +0200

    last update

--- CSS/CardMemoryGame/js/app.js
+++ CSS/CardMemoryGame/js/app.js
@@ -1,9 +1,13 @@
+'use strict';
+import { cardBackground } from 'app/data.js'
+
 let card = document.querySelector('.card');
 const cardsList = document.querySelector('.flex-container');
 
 
 
 function cardFlip(e) {
+    
     if (e.target.parentElement.classList.contains('card')) {
         e.target.parentElement.classList.toggle('flip');
     }
@@ -15,7 +19,21 @@ function cardFlip(e) {
 
 //document.body.addEventListener('click', cardFlip);
 
-cardsList.addEventListener('click', cardFlip);
+
+function checkCardMatch(element){
+
+    e.target.parentElement.classList.add('selected');
+    
+    if(e.target.parentElement.classList.contains()) {
+
+    }
+}
+
+cardsList.addEventListener('click', function(){
+    cardFlip();
+    console.log(cardBackground);
+
+});
 
 
 
